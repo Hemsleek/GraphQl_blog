fa3788248f22d8bf98791d013c7366a423f02acd
asiyanbi mubashir <asiyanbimubashir@gmail.com>
2021-06-14 23:13:09 +0100

    feat:all books query

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,26 @@
-
 import React from 'react'
+import {useQuery, gql} from '@apollo/client'
 
+
+const ALL_BOOKS = gql`
+  query{
+    allBooks{
+      title
+      author
+      published
+    }
+  }
+`
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if(result.loading) return (
+    <div>Loading...</div>
+  )
 
   return (
     <div>
@@ -23,7 +37,7 @@ const Books = (props) => {
               Published
             </th>
           </tr>
-          {books.map(a =>
+          {result.data.allBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
